59eea19fee210c89b31081a98c3860a368877d87
Christoph Wagner <wgc.privat@gmail.com>
2014-05-03 02:26:21 +0200

    enabled debugging

--- config.inc.php
+++ config.inc.php
@@ -5,8 +5,9 @@
  */
 
 // Root target of your roundcube installation
-$rcmail_config['fetchmail_rc_roundcube_install_path'] = "/usr/share/igestis/modules/Roundcube/roundcubemail/";
+$rcmail_config['fetchmail_rc_roundcube_install_path'] = "/var/lib/roundcube/";
+//$rcmail_config['fetchmail_rc_roundcube_install_path'] = "/usr/share/roundcube/";
 // Error count before to warn the user by mail that his mailbox is not working anymore
 $rcmail_config['fetchmail_rc_errors_before_warning'] = 5;
 
-define("CONSOLE_DEBUG_MODE", false);
+define("CONSOLE_DEBUG_MODE", true);
